65bb5d98f25eca1a03dd7851f9bc92d690f9c452
pandaForce <63492114+pandaForce@users.noreply.github.com>
2021-05-30 13:01:30 +1000

    format date fields using normal js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,24 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // console.log('props: '+props.JSON.Stringify());
+  const month = props.date.toLocaleString("en-US", { month: 'long' });
+  const day = props.date.toLocaleString("en-US", { day: '2-digit' });
+  const year = props.date.getFullYear();
 
-    console.log('props: '+props);
-
-    return (
-<div className="expense-item">
-    <div>{props.date.toISOString()}</div>
-    <div className="expense-item__description">
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-</div>
-
-    )
-
+  );
 }
 
 export default ExpenseItem;
