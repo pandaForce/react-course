abdec3c1a51541f01d080dce52e7043cb736b27b
pandaForce <63492114+pandaForce@users.noreply.github.com>
2021-05-30 12:13:21 +1000

    pass data to jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
 
+    const expenseDate = new Date(2021, 2, 21);
+    const expenseTitle = 'Car Insurance222';
+    const expenseAmount = 20000.5;
+
     return (
 <div className="expense-item">
-    <div>Mar-28-2021</div>
+    <div>{expenseDate.toISOString()}</div>
     <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.5</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
     </div>
 </div>
 
